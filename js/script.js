ae9f0dc4d208800b38a2971b8818797de9392f87
Станислав <gerrymoineau@gmail.com>
2018-04-01 17:08:39 +0500

    Откорректировал по perfect pixel, оживил слайдеры

--- js/script.js
+++ js/script.js
@@ -25,3 +25,33 @@ mapClose.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	mapPopup.classList.remove("modal-show");
 });
+
+var slides = document.querySelectorAll(".slider-item");
+var slideBtn = document.querySelectorAll(".btn-slide");
+
+for (i = 0; i < slideBtn.length; i++) {
+	slideBtn[i].index = i;
+	slideBtn[i].addEventListener("click", function() {
+		var activeBtn = document.querySelector(".active");
+		activeBtn.classList.remove("active");
+		this.classList.add("active");
+		var activeSlide = document.querySelector(".slide-show");
+		activeSlide.classList.remove("slide-show");
+		slides[this.index].classList.add("slide-show");
+	});
+}
+
+var services = document.querySelectorAll(".services-description-item");
+var serviceBtn = document.querySelectorAll(".btn-services");
+
+for (i = 0; i < serviceBtn.length; i++) {
+	serviceBtn[i].index = i;
+	serviceBtn[i].addEventListener("click", function() {
+		var currentBtn = document.querySelector(".current");
+		currentBtn.classList.remove("current");
+		this.classList.add("current");
+		var selectedService = document.querySelector(".selected");
+		selectedService.classList.remove("selected");
+		services[this.index].classList.add("selected");
+	});
+}
